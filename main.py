26324367128ac803228d42913f3b296363050a8a
SteelingCow437 <79171525+SteelingCow437@users.noreply.github.com>
2022-01-18 08:47:55 -0500

    wiped the meme code

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def on_button_pressed_a():
+    basic.show_string("t's about drive, it's about power We stay hungry, we devour Put in the work, put in the hours and take what's ours (ahoo) Black and Samoan in my veins My culture bangin' with Strange I change the game, so what's my motherfuckin' name (Rock)")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def on_forever():
     pass
 basic.forever(on_forever)
