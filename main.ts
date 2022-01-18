c4a788840f6a8865eb4eb32b5edd4b037d521b3d
SteelingCow437 <79171525+SteelingCow437@users.noreply.github.com>
2022-01-18 13:37:45 -0500

    the rock

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("t's about drive, it's about power We stay hungry, we devour Put in the work, put in the hours and take what's ours (ahoo) Black and Samoan in my veins My culture bangin' with Strange I change the game, so what's my motherfuckin' name (Rock)")
 })
-basic.forever(function () {
-	
-})
